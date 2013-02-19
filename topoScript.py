ac8bfb3ab5d8eec07de8c74b12d2d54976b6dd84
paulinev <paulinev@mit.edu>
2013-02-19 15:31:02 -0500

    Update topoScript.py
    At this point we can:
    ping h2 from h1
    ping h1 from h2
    ping h3 from h2
    ping 10.0.2.2 (but not h2) from h3
    
    Still need: ping h1/h3 from h3/h1
    ping h2 from h3

--- topoScript.py
+++ topoScript.py
@@ -11,20 +11,29 @@ class ProtoTester(Topo):
         Topo.__init__(self)
 
         # Add hosts and switches
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
+        h1 = self.addHost('h1', ip='10.0.1.1')
+        h2 = self.addHost('h2', ip='10.0.1.2')
+        h3 = self.addHost('h3', ip='10.0.2.1')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
-
-  	#h1.setIP('10.0.1.1', 24)
-	#h2.setIP('10.0.2.1', 24)
-	#h3.setIP('10.0.3.1', 24)
 
         # Add links
         self.addLink(h1, s1)
         self.addLink(s1, h2)
         self.addLink(h2, s2)
         self.addLink(s2, h3)
+        
+        #h1.setHostRoute('h2', 'h1-eth0')
+	#h1.setHostRoute('h3', 'h1-eth0')
+        #h2.setHostRoute('h1', 'h2-eth0')
+	#h2.setHostRoute('h3', 'h2-eth1')
+        #h3.setHostRoute('h2', 'h3-eth0')
+        #h3.setHostRoute('h1', 'h3-eth0')
 
 topos = {'prototester': (lambda: ProtoTester())}
+
+# From command line run (how to integrate into script?):
+# h1 ifconfig h1-eth0 10.0.1.1 netmask 255.255.255.0
+# h2 ifconfig h2-eth0 10.0.1.2 netmask 255.255.255.0
+# h2 ifconfig h2-eth1 10.0.2.2 netmask 255.255.255.0
+# h3 ifconfig h3-eth0 10.0.2.1 netmask 255.255.255.0
