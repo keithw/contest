1ebaa132a88a624a4165b9b6faffa1e4a9e0a9e1
paulinev <paulinev@mit.edu>
2013-02-22 11:31:25 -0500

    Integrate command-line prompts into Python script

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-#Which of these statements do I need/are there more that should be here?
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.node import Host
+from mininet.link import Link
 from mininet.cli import CLI
-from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
 
 from subprocess import Popen, PIPE
 from time import sleep, time
@@ -13,24 +14,111 @@ import sys
 import os
 import math
 
-#How do I set up the network to be prototester?
-net = Mininet(topo=topo, host=Host, link=TCLink)
+#Topology to run cellSim
+#H1--S1--H2*--S2--H3
+class ProtoTester(Topo):
+    def __init__(self):
+        
+        # Initialise topology
+        Topo.__init__(self)
 
-def set_all_IP(net):
+        # Add hosts and switches
+        h1 = self.addHost('h1', ip='10.0.1.1')
+        h2 = self.addHost('h2', ip='10.0.1.2')
+        h3 = self.addHost('h3', ip='10.0.2.1')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Add links
+        self.addLink(h1, s1)
+        self.addLink(s1, h2)
+        self.addLink(h2, s2)
+        self.addLink(s2, h3)
+
+def set_all_IP(net, h1, h2, h3):
+    h1.sendCmd('ifconfig h1-eth0 10.0.1.1 netmask 255.255.255.0')
+    h1.waitOutput()
+    h2.sendCmd('ifconfig h2-eth0 10.0.1.2 netmask 255.255.255.0')
+    h2.waitOutput()
+    h2.sendCmd('ifconfig h2-eth1 10.0.2.2 netmask 255.255.255.0')
+    h2.waitOutput()
+    h3.sendCmd('ifconfig h3-eth0 10.0.2.1 netmask 255.255.255.0')
+    h3.waitOutput()
+
+def set_all_routes(net, h1, h2, h3):
+    h1.sendCmd("setHostRoute('h2', 'h1-eth0')")
+    h1.waitOutput()
+    h1.sendCmd("setHostRoute('h3', 'h1-eth0')")
+    h1.waitOutput()
+    h2.sendCmd("setHostRoute('h1', 'h2-eth0')")
+    h2.waitOutput()
+    h2.sendCmd("setHostRoute('h3', 'h2-eth1')")
+    h2.waitOutput()
+    h3.sedCmd("setHostRoute('h2', 'h3-eth0')")
+    h3.waitOutput()
+    h3.sendCmd("setHostRoute('h1', 'h3-eth0')")
+    h3.waitOutput()
+
+def run_cellsim(net):
+    h2 = net.getNodeByName('h2')
+    #What should go in the http line?
+    proc = h2.popen("c http/github.com/keithw/multisend/blob/master/sender/cellsim.cc", shell=True)
+    sleep(1)
+    return [proc]
+
+def stop_cellsim():
+    Popen('killall -9 cat', shell=True).wait()
+
+def start_ping(net, h1, h2, h3):
+    print 'Pinging h2 at 10.0.1.2 from h1...'
+    h1.sendCmd('ping -c 5 10.0.1.2')
+    print h1.waitOutput()
+    print 'Pinging h3 at 10.0.2.1 from h2...'
+    h2.sendCmd('ping -c 5 10.0.2.1')
+    print h2.waitOutput()
+    print 'Pinging h1 at 10.0.1.1 from h3...'
+    h3.sendCmd('ping -c 5 10.0.1.1')
+    print h3.waitOutput()
+
+def display_routes(net, h1, h2, h3):
+    print 'h1 route...'
+    h1.sendCmd('route')
+    print h1.waitOutput()
+    print 'h2 route...'
+    h2.sendCmd('route')
+    print h2.waitOutput()
+    print 'h3 route...'
+    h3.sendCmd('route')
+    print h3.waitOutput()
+
+def test_cellsim():
+    topo = ProtoTester()
+    net = Mininet(topo=topo, host=Host, link=Link)
+    net.start()
+
     h1 = net.getNodeByName('h1')
     h2 = net.getNodeByName('h2')
     h3 = net.getNodeByName('h3')
 
-    h1.sendCmd("ifconfig h1-eth0 10.0.1.1 netmask 255.255.255.0")
-    h1.waitOutput()
-    h2.sendCmd("ifconfig h2-eth0 10.0.1.2 netmask 255.255.255.0")
-    h2.waitOutput()
-    h2.sendCmd("ifconfig h2-eth1 10.0.2.2 netmask 255.255.255.0")
-    h3.sendCmd("ifconfig h3-eth0 10.0.2.1 netmask 255.255.255.0")
+    set_all_IP(net, h1, h2, h3)
+##    set_all_routes(net, h1, h2, h3)
+    
+    #Dump connections
+    dumpNodeConnections(net.hosts)
+    #Ping all pairs
+    net.pingAll()
 
-def run_Cellsim(net):
-    h2 = net.getNodeByName('h2')
-    #What should go in the http line?
-    proc = h2.popen("python http/whatshouldgohere.py", shell=True)
-    sleep(1)
-    return [proc]
+    print "In case that didn't work..."
+    start_ping(net, h1, h2, h3)
+
+    display_routes(net, h1, h2, h3)
+##    run_cellsim(net)
+##    sleep(5)
+    CLI(net)
+
+##    stop_cellsim()
+    net.stop()
+##    Popen('pgrep -f cellsim.cc | xargs kill -9', shell=True).wait()
+
+if __name__ == '__main__':
+    test_cellsim()
