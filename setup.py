844c5d04d63c779773df6a63583be315ccd12ac0
paulinev <paulinev@mit.edu>
2013-02-26 19:44:33 -0500

    Will run cellsim

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
+## TO DO: run sudo ./cellsim <uplink> <downlink> <h3 mac> <error> from xterm
+
+## To run cellsim on this network: first, run this code. Then open xterms
+## for all hosts by entering 'xterm h1 h2 h3' into the mininet prompt.
+## In the xterm for h3 (the client host), enter the command 'ifconfig'
+## and locate the host's MAC address.
+## In the xterm for h2, cd into the directory where cellsim is stored,
+## then run 'sudo ifconfig h2-eth0 up promisc' and 'sudo ifconfig h2-eth1
+## up promisc' to set both ethernet ports to promiscuous mode. Next run
+## 'sudo ./cellsim <uplink file> <downlink file> <client MAC address> 0.0'
+## to start cellsim.
+
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Host
@@ -19,6 +31,7 @@ ensureRoot()
 
 #Topology to run cellSim
 #H1--S1--H2*--S2--H3
+
 class ProtoTester(Topo):
     def __init__(self):
         
@@ -49,20 +62,33 @@ def set_all_IP(net, h1, h2, h3):
     h3.waitOutput()
 
 def add_default_routes(net, h1, h3):
-    h1.sendCmd('sudo route add default gw 10.0.2.1')
+    h1.sendCmd('sudo route add default gw 10.0.1.2')
     h1.waitOutput()
     h3.sendCmd('sudo route add default gw 10.0.2.2')
     h3.waitOutput()
 
+# Want this to run in an xterm for h2... is there a way to make that happen?
 def run_cellsim(net):
+    # Get user data for uplink and downlink files; get client MAC
+    uplink = raw_input('Enter uplink filepath: ')
+    downlink = raw_input('Enter downlink filepath: ')
+    #clientMAC = raw_input('Enter client MAC address (in this setup,\
+    #        H3 is the client): ')
+    h3 = net.getNodeByName('h3')
+    clientMAC = h3.MAC()
+    
     h2 = net.getNodeByName('h2')
-    #What should go in the http line?
-    proc = h2.popen("c http/github.com/keithw/multisend/blob/master/sender/cellsim.cc", shell=True)
-    sleep(1)
-    return [proc]
+    h2.sendCmd('sudo ifconfig h2-eth0 up promisc')
+    h2.waitOutput()
+    h2.sendCmd('sudo ifconfig h2-eth1 up promisc')
+    h2.waitOutput()
+    startCellsim = 'sudo ./cellsim %(uplink)s %(downlink)s %(mac)s 0.0'%\
+            {'uplink': uplink, 'downlink': downlink, 'mac': clientMAC}
+    h2.cmd(startCellsim)
 
 def stop_cellsim():
     Popen('killall -9 cat', shell=True).wait()
+    Popen('sudo mn -c', shell=True).wait() 
 
 def start_ping(net, h1, h2, h3):
     print 'Pinging h2 at 10.0.1.2 from h1...'
@@ -103,12 +129,16 @@ def test_cellsim():
     #Ping all pairs
     net.pingAll()
 
-    print "In case that didn't work..."
-    start_ping(net, h1, h2, h3)
+#    print "In case that didn't work..."
+#    start_ping(net, h1, h2, h3)
 
     display_routes(net, h1, h2, h3)
-##    run_cellsim(net)
-##    sleep(5)
+#    run_cellsim(net)
+#    sleep(5)
+#    print "On mininet prompt, open xterms for H1, H2, and H3 by entering: \
+#            xterm h1 h2 h3 . \
+#            To run cellsim, switch to h2's xterm and enter: \
+#            sudo run_cellsim.py"
     CLI(net)
 
 ##    stop_cellsim()
